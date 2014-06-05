640d6ac4e3f0c3d995f9ba434416c6b432514231
timcolonel <timothee.guerin@outlook.com>
2014-06-05 15:32:31 +0100

    setup platform

--- lib/wow/package/platform.rb
+++ lib/wow/package/platform.rb
@@ -1,7 +1,31 @@
 module Wow
   module Package
     class Platform
+      attr_accessor :name
       
+      # Return true is the given platform is a parent or equals to this 
+      # i.e Self is a subset of the given platform.
+      # @param platform Other platform object to test
+      #
+      # e.g.
+      # win32.is?(windows) => true
+      # windows.is?(win32) => false
+      # windows.is?(windows) => true
+      def is?(platform)
+        fail ArgumentError unless platform.is_a? Wow::Package::Platform
+      end
+
+      # This this the opposite of is?. Return true if the given platorm is a child or equals to this.
+      # i.e The given platform is a subset of self
+      # @param platform Other platform object to test
+      #
+      # e.g.
+      # windows.include?(win32) => true
+      # win32.include?(windows) => false
+      # windows.include?(windows) => true
+      def include?(platform)
+        fail ArgumentError unless platform.is_a? Wow::Package::Platform
+      end
     end
   end
 end
