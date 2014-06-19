17efaa77a9dfe10650f3cbd4b71abecc1edc487e
Timothee Guerin <timothee.guerin@outlook.com>
2014-06-19 22:39:36 +0100

    removed python

--- python_old/wow/src/exception.py
+++
@@ -1,5 +0,0 @@
-class WowException(Exception):
-    """
-        Execption used to display error message when command is run
-    """
-    pass
--- python_old/wow/src/utils.py
+++
@@ -1,6 +0,0 @@
-import os
-from src.package import Package
-
-def list_subdir(dir):
-    return [name for name in os.listdir(dir)
-            if os.path.isdir(os.path.join(dir, name))]
