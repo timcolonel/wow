f6a8f987e456583b60929528742dedfdecc9245b
timcolonel <timothee.guerin@outlook.com>
2014-07-16 20:50:44 -1200

    oijoijoi

--- lib/wow/builder.rb
+++ lib/wow/builder.rb
@@ -3,6 +3,7 @@ module Wow
     def initialize(directory, filename = 'wow.json',platform = :any)
       config = Wow::Package::Config.new(platform)
       archive_path = config.create_archive
+      puts "Created archive in #{archive_path"
     end
   end
 end
