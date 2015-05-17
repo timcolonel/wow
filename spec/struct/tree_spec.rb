517535c490979018c9f7859ed98d2c5ae8ed596a
Timothee Guerin <timothee.guerin@outlook.com>
2015-05-17 19:43:41 +0100

    more spec

--- spec/struct/tree_spec.rb
+++ spec/struct/tree_spec.rb
@@ -10,5 +10,21 @@ RSpec.describe Tree do
       it { expect(subject.name).to eq(key) }
       it { expect(subject.children).to be_empty }
     end
+
+    context 'when argument is a hash with name and children format' do
+      let (:hash) { {name: 'root', children:
+          [{name: 'child1'},
+           {name: 'child2',
+            children: [
+                {name: 'sub_child1'},
+                {name: 'sub_child2'}
+            ]}]} }
+
+      subject { Tree.new(hash) }
+
+      it { expect(subject.name).to eq('root') }
+      it { expect(subject.children.size).to eq(2) }
+
+    end
   end
 end
